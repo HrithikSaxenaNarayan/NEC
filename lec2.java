d68162bee2e8b6f5861403f85482f383e0c921ea
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-07-30 11:54:36 +0530

    update

--- lec2.java
+++ lec2.java
@@ -11,7 +11,25 @@ public class lec2 {
         }
     }
 
+    public static int power(int n) {
+        int pow = 1;
+        while (n != 0) {
+            n /= 10;
+            pow *= 10;
+        }
+        return pow / 10;
+    }
+
+    public static void forwardOrder(int n) {
+        int pow = power(n);
+        while(n != 0) {
+            int ans = n / pow;
+            System.out.println(ans);
+            n %= pow;
+            pow /= 10;
+        }
+    }
     public static void main(String[] args) {
-        reverseOrder(scn.nextInt());
+        forwardOrder(scn.nextInt());
     }
 }
