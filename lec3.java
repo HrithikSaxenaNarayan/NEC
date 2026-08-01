cb754134e69510f0089bc11f822bef808c8ecccb
HrithikSaxenaNarayan <mrsaxena77777@gmail.com>
2026-08-01 12:45:31 +0530

    Update lec3.java

--- lec3.java
+++ lec3.java
@@ -39,6 +39,35 @@ public class lec3 {
         }
     }
 
+    public static void numW(int n) {
+        int nst = 1;
+        int nsp = ((2 * n) - 3);
+        for (int row = 1; row <= n; row++) {
+            int val = 1;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(val++ + "\t");
+            }
+
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+
+            for (int cst = 1; cst <= nst; cst++) {
+                if (row == n && cst == 1) {
+                    val--;
+                    continue;
+                }
+                System.out.print(--val + "\t");
+            }
+
+            nst++;
+            nsp -= 2;
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
         binomialPattern(scn.nextInt());
     }
