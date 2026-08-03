bbed9f2f50caa5e4a397badb6829789c3d21a24f
HrithikSaxenaNarayan <mrsaxena77777@gmail.com>
2026-08-03 14:46:47 +0530

    Update lec4.py

--- lec4.py
+++ lec4.py
@@ -25,4 +25,36 @@ def binary_to_decimal(n):
     return ans
 
 
+def add_two_arrays(arr, brr):
+    n = len(arr)
+    m = len(brr)
 
+    ans_len = max(n, m) + 1
+    ans = [0] * ans_len
+
+    i = n - 1
+    j = m - 1
+    k = ans_len - 1
+    carry = 0
+
+    while k >= 0:
+        total = carry
+
+        if i >= 0:
+            total += arr[i]
+
+        if j >= 0:
+            total += brr[j]
+
+        carry = total // 10
+        ans[k] = total % 10
+
+        i -= 1
+        j -= 1
+        k -= 1
+
+    
+    start = 1 if ans[0] == 0 else 0
+
+    for idx in range(start, ans_len):
+        print(ans[idx], end="\t")
