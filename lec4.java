2a57cf28c94d071fd52986b49e116ba44d62ccd7
HrithikSaxenaNarayan <mrsaxena77777@gmail.com>
2026-08-03 11:08:26 +0530

    Update lec4.java

--- lec4.java
+++ lec4.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class lec4{
+    public static Scanner scn = new Scanner(System.in);
 
+    public static int decimalToBinary(int n) {
+        int ans = 0;
+        int pow = 1;
+        while (n != 0) {
+            int rem = n % 2;
+            n /= 2;
+            ans += rem * pow;
+            pow *= 10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] ARGS) {
+        decimal to binary(scn.nextInt());
+    }
+}
