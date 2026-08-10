8db16f2b15da3ac7f912900fe93d560bf0771717
HrithikSaxenaNarayan <mrsaxena77777@gmail.com>
2026-08-10 14:42:32 +0530

    Update lec4.py

--- lec4.py
+++ lec4.py
@@ -1,60 +1,155 @@
-def decimal_to_binary(n):
-    ans = 0
-    pow = 1
+class NumberSystem3:
 
-    while n != 0:
-        rem = n % 2
-        n //= 2
-        ans += rem * pow
-        pow *= 10
+    @staticmethod
+    def numFrequency(n, data):
+        count = 0
 
-    return ans
+        while n > 0:
+            lastDig = n % 10
+
+            if lastDig == data:
+                count += 1
+
+            n //= 10
+
+        return count
+
+    @staticmethod
+    def numFrequencyQueries(n, query):
+        ans = [0] * 10
+
+        while n != 0:
+            d = n % 10
+            n //= 10
+
+            ans[d] += 1
+
+        for q in query:
+            print(q, ":", ans[q])
+
+    @staticmethod
+    def pow(n):
+        pwr = 1
+
+        while n != 0:
+            n //= 10
+            pwr *= 10
+
+        return pwr
+
+    @staticmethod
+    def decimalToBinary(n):
+        ans = 0
+        power = 1
+
+        while n != 0:
+            rem = n % 2
+            n //= 2
+
+            ans += rem * power
+            power *= 10
+
+        return ans
+
+    @staticmethod
+    def binaryToDecimal(n):
+        pwr = 1
+        ans = 0
+
+        while n != 0:
+            rem = n % 10
+
+            ans += rem * pwr
+            pwr *= 2
+            n //= 10
+
+        return ans
+
+    @staticmethod
+    def decimalToAnyBase(n, base):
+        pwr = 1
+        ans = 0
+
+        while n != 0:
+            rem = n % base
+
+            ans += rem * pwr
+
+            n //= base
+            pwr *= 10
+
+        return ans
+
+    @staticmethod
+    def anyBaseToDecimal(n, base):
+        pwr = 1
+        ans = 0
+
+        while n != 0:
+            rem = n % 10
+
+            ans += rem * pwr
+
+            n //= 10
+            pwr *= base
+
+        return ans
+
+    @staticmethod
+    def anyBaseToAnyBase(n, b1, b2):
+        decimal = NumberSystem3.anyBaseToDecimal(n, b1)
+
+        return NumberSystem3.decimalToAnyBase(decimal, b2)
+
+    @staticmethod
+    def anyBaseAddition(n, m, base):
+        pwr = 1
+        ans = 0
+        carry = 0
+
+        while n != 0 or m != 0 or carry != 0:
+
+            sum_val = carry + (n % 10) + (m % 10)
+
+            n //= 10
+            m //= 10
+
+            ansLastDig = sum_val % base
+            carry = sum_val // base
+
+            ans += ansLastDig * pwr
+            pwr *= 10
+
+        return ans
+
+    @staticmethod
+    def anyBaseSub(n, m, base):
+        pwr = 1
+        ans = 0
+        borrow = 0
+
+        while n != 0:
+
+            diff = borrow + (n % 10) - (m % 10)
+
+            n //= 10
+            m //= 10
+
+            if diff < 0:
+                borrow = -1
+                diff += base
+            else:
+                borrow = 0
+
+            ans += diff * pwr
+            pwr *= 10
+
+        return ans
 
 
+# Main
+n = int(input())
+m = int(input())
+base = int(input())
 
-def binary_to_decimal(n):
-    pwr = 1
-    ans = 0
-
-    while n != 0:
-        rem = n % 10
-        ans += rem * pwr
-        pwr *= 2
-        n //= 10
-
-    return ans
-
-
-def add_two_arrays(arr, brr):
-    n = len(arr)
-    m = len(brr)
-
-    ans_len = max(n, m) + 1
-    ans = [0] * ans_len
-
-    i = n - 1
-    j = m - 1
-    k = ans_len - 1
-    carry = 0
-
-    while k >= 0:
-        total = carry
-
-        if i >= 0:
-            total += arr[i]
-
-        if j >= 0:
-            total += brr[j]
-
-        carry = total // 10
-        ans[k] = total % 10
-
-        i -= 1
-        j -= 1
-        k -= 1
-
-    
-    start = 1 if ans[0] == 0 else 0
-
-    for idx in range(start, ans_len):
-        print(ans[idx], end="\t")
+print(NumberSystem3.anyBaseSub(n, m, base))
