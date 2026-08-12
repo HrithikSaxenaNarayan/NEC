22c675695fa0bc5d2174b6bd9518dec0f2240a81
HrithikSaxenaNarayan <mrsaxena77777@gmail.com>
2026-08-12 15:34:03 +0530

    Update lec10.java

--- lec10.java
+++ lec10.java
@@ -39,6 +39,67 @@ public class lec10 {
         System.out.println(sb.toString());
 
     }
+
+    public int scoreOfStringLeetCode3110(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int score = 0;
+
+        for (int i = 0; i < sb.length() - 1; i++) {
+            int diff = Math.abs(sb.charAt(i) - sb.charAt(i + 1));
+            score += diff;
+        }
+
+        return score;
+    }
+
+
+    public boolean hasAdjacentDigitDifferenceLeetCode3931(int n) {
+        StringBuilder sb = new StringBuilder(String.valueOf(Math.abs(n)));
+
+        for (int i = 0; i < sb.length() - 1; i++) {
+            int digit1 = sb.charAt(i) - '0';
+            int digit2 = sb.charAt(i + 1) - '0';
+
+            if (Math.abs(digit1 - digit2) != 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+public static String funnyStringHackerRank(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    StringBuilder rev = new StringBuilder(s).reverse();
+
+    for (int i = 0; i < sb.length() - 1; i++) {
+        int forward = Math.abs(sb.charAt(i) - sb.charAt(i + 1));
+        int reverse = Math.abs(rev.charAt(i) - rev.charAt(i + 1));
+
+        if (forward != reverse) {
+            return "Not Funny";
+        }
+    }
+
+    return "Funny";
+}
+
+public static String superReducedStringHackerRank(String s) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s.charAt(i);
+
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ch) {
+            sb.deleteCharAt(sb.length() - 1);
+        } else {
+            sb.append(ch);
+        }
+    }
+
+    return sb.length() == 0 ? "Empty String" : sb.toString();
+}
+
     public static void main(String[] args) {
         stringBuilderOps();
     }
