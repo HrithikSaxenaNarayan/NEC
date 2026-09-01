a166b046a111180fd166f08c42a05b6a3c53cf74
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-09-01 12:12:51 +0530

    update on HashMap

--- lec12.java
+++ lec12.java
@@ -39,7 +39,49 @@ public class lec12{
         return map;
     }
 
+    public static HashMap<Character, Integer> charFreq(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (Integer i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        return map;
+    }
+    
+    public static void nonRepeating387(ArrayList<Integer> arrLst) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Integer i = 0; i < arrLst.size(); i++) {
+            map.put(arrLst.get(i), map.getOrDefault(arrLst.get(i), 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entries : map.entrySet()) {
+            // System.out.println(entries.getKey() + ": " + entries.getValue());
+            if (entries.getValue() > 1) {
+                continue;
+            } else {
+                System.out.println(entries.getKey());
+                break;
+            }
+        }
+    }
+
+    public static int findDuplicate442(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int duplicateElem = -1;
+        for (Integer elem : nums) {
+            map.put(elem, map.getOrDefault(elem, 0) + 1);
+        }
+        for (Integer i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) > 1) {
+                duplicateElem = nums[i];
+            }
+        }
+        return duplicateElem;
+    }
+
+    
+    
     public static void main(String[] args){
-        displayGen(countFreq(arrToMap(scn.nextInt())));
+        // displayGen(countFreq(arrToMap(scn.nextInt())));
+        displayGen(charFreq("abaacbacc cabc abc"));
     }
 }
--- lec12.py
+++ lec12.py
@@ -40,6 +40,39 @@ def count_freq(arr):
     return map_data
 
 
+def non_repeating_387(arr_lst):
+    freq = {}
+
+    # Count frequency
+    for value in arr_lst:
+        freq[value] = freq.get(value, 0) + 1
+
+    # Find first non-repeating element
+    for value in arr_lst:
+        if freq[value] > 1:
+            continue
+        else:
+            print(value)
+            break
+
+
+def find_duplicate_442(nums):
+    freq = {}
+    duplicate_elem = -1
+
+    # Count frequency
+    for elem in nums:
+        freq[elem] = freq.get(elem, 0) + 1
+
+    # Find duplicate
+    for elem in nums:
+        if freq[elem] > 1:
+            duplicate_elem = elem
+
+    return duplicate_elem
+
+
+
 # main
 n = int(input())
 
