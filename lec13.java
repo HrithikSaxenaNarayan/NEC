c1787014571195906b2d7407108aaf774db39a9a
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-09-09 11:58:14 +0530

    updates on recursion

---
+++ lec13.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class lec13{
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int[] inputArr(int n, int val){
+        if (n == 0) {
+            return new int[val];
+        }
+
+        int[] arr = inputArr(n-1, val);
+        arr[n-1] = scn.nextInt();
+
+        return arr;
+    }
+
+
+    public static void printArr(int[] arr, int idx){
+        if (idx == arr.length) {
+            return;
+        }
+        System.out.print(arr[idx] + "\t");
+        printArr(arr, idx + 1);
+    }
+
+    public static void main(String[] args){
+        printArr(inputArr(scn.nextInt(), scn.nextInt()), 0);
+    }
+}
